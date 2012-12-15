62cc5d543e143aa4616f18fc5f6c30d8d600f9ec
Paul Zaich <pzaich@gmail.com>
2012-12-15 22:24:22 +0900

    letter in place

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(function () {
+  $('.bauble').hover(function () {
+    $(this).animate({ marginTop : '0'});
+  },
+  function () {
+    $(this).animate({ marginTop : '-30px'});
+  });
 
+  $('.read-story').click(function () {
+    $(this).hide();
+    $('.story').fadeIn(2000);
+  });
+});
